a73b7c2ff5cec74a69ad23a947087ca7a9053e1c
AbhayChauhan111 <abhay.up20@gmail.com>
2026-06-14 15:15:33 +0530

    Add airthmetic_Operator.py

---
+++ 02_Operators/airthmetic_opeartaor.py
@@ -0,0 +1,29 @@
+#addition 
+a=10
+b=20
+print("The sum of a nad b is :",a+b)
+
+#subtraction
+a=20
+b=30
+print("The difference of a and b is :",a-b)
+
+#multiplication
+a=10
+b=20
+print("The product of a and b is :",a*b)
+
+#division(float)
+a=20
+b=10
+print("The quotient of a and b is :",a/b) #returns float value
+
+#division(floor)
+a=20
+b=10
+print("The floor quotient of a and b is :",a//b) #returns floor value
+
+#modulus
+a=20
+b=10
+print("The remainder of a and b is :",a%b) #returns remainder value
