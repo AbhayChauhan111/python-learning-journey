81b0a373b66f47524ee32a9690d56cc143d27cac
AbhayChauhan111 <abhay.up20@gmail.com>
2026-06-14 14:49:16 +0530

    Add Python input examples

---
+++ 01_Basics/input.py
@@ -0,0 +1,10 @@
+#taking input from user
+a=input("Enter your name:") 
+#input()function is used to take input from user and it always returns string value
+
+b=int(input("Enter your age:")) #if we want to take integer input from user then we have to typecast
+
+c=float(input("Enter your height:")) #if we want to take input in float then we have to typecast
+print("your name is :",a)
+print("your age is:",b)
+print("your height is:",c)
