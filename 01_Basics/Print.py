53351523fc1a46887944aeb89450cb8db1222838
AbhayChauhan111 <abhay.up20@gmail.com>
2026-06-14 14:59:57 +0530

    Update Print.py with new examples

--- 01_Basics/Print.py
+++ 01_Basics/Print.py
@@ -6,3 +6,11 @@ age = 30
 print("You are " + str(age) + " years old.")
 pi = 3.14159
 print("The value of pi is approximately " + str(pi) + ".")
+
+
+"""Given two inputs that are store in variables a and b, you need to print a and b in a single line with a space separating them.
+Add new line after printing with space."""
+
+a="Hello"
+b="Word"
+print(a, b)
